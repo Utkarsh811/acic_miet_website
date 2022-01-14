1cab415b73370558a0cc607664a9fe304bbe0d41
k-nautresurrect <kushignite@gmail.com>
2022-01-14 18:32:27 +0530

    Added Home component to app

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,11 @@
-import "./App.css";
 import NavBar from "./components/navbar/NavBar";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Home />
     </div>
   );
 }
