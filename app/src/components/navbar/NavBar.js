e16e363d6f0c405547faaf5eee3ecc4ddaa1797a
k-nautresurrect <kushignite@gmail.com>
2022-01-14 14:13:08 +0530

    Added responsive navbar

---
+++ app/src/components/navbar/NavBar.js
@@ -0,0 +1,116 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useTheme } from "@mui/material/styles";
+
+import acicMiet from "../../assets/acicMiet.png";
+import acicMietPhone from "../../assets/acicMietPhone.png";
+
+const pages = [
+  "Services",
+  "Outcome",
+  "Apply",
+  "Careers",
+  "Contact",
+  "Web mail",
+];
+
+const NavBar = (props) => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const theme = useTheme();
+  const isPhone = useMediaQuery(theme.breakpoints.down("sm"));
+  console.log(isPhone);
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleCloseMenu = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <AppBar
+      position="fixed"
+      color="transparent"
+      sx={{ backdropFilter: "blur(30px)" }}
+    >
+      <Container maxWidth="xl">
+        <Toolbar style={{ justifyContent: "space-between" }} disableGutters>
+          {isPhone ? (
+            <img
+              src={acicMietPhone}
+              alt="acicLogo"
+              style={{ width: "10rem" }}
+            />
+          ) : (
+            <img src={acicMiet} alt="acicLogo" style={{ width: "18%" }} />
+          )}
+          <div>
+            {isPhone ? (
+              <Box>
+                <IconButton
+                  size="large"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleMenu}
+                  color="inherit"
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorEl}
+                  anchorOrigin={{
+                    vertical: "bottom",
+                    horizontal: "left",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "left",
+                  }}
+                  open={Boolean(anchorEl)}
+                  onClose={handleCloseMenu}
+                  sx={{
+                    display: { xs: "block", md: "none" },
+                  }}
+                >
+                  {pages.map((page) => (
+                    <MenuItem key={page} onClick={handleCloseMenu}>
+                      <Typography textAlign="center">{page}</Typography>
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </Box>
+            ) : (
+              <Box style={{ flex: 1, justifyContent: "space-evenly" }}>
+                {pages.map((page, index) => (
+                  <Button
+                    key={index}
+                    varient="text"
+                    color="inherit"
+                    size="small"
+                  >
+                    {page}
+                  </Button>
+                ))}
+              </Box>
+            )}
+          </div>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+export default NavBar;
