2eb7985631be7e3da287afb78b376604145b4a78
k-nautresurrect <kushignite@gmail.com>
2022-02-04 12:43:56 +0530

    Updated banner

--- app/src/components/baner/Banner.js
+++ app/src/components/baner/Banner.js
@@ -5,7 +5,17 @@ import acicMietPhone from "../../assets/acicMietPhone.png";
 
 const Banner = (props) => {
   return (
-    <div className="Logo">
+    <div
+      className="Banner"
+      style={{
+        display: "flex",
+        width: "100vw",
+        height: "100vh",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
       <img src={acicMiet} alt="acicLogo" style={{ width: "20rem" }} />
     </div>
   );
